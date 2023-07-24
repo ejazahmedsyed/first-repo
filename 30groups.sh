85d3471cb2cc53112bfd10acdfa1f2d48b8ac901
ejazahmedsyed <135770515+ejazahmedsyed@users.noreply.github.com>
2023-07-24 00:31:18 -0400

    Update 30groups.sh

--- 30groups.sh
+++ 30groups.sh
@@ -1,7 +1,6 @@
 for((group=01;group<=30;group++))
 do
-echo "Azure_Devops_$group"
-az group delete -n Azure_Devops_$group --yes #--query "{name:name}"
+echo "Azure_Devops_$group"up delete -n Azure_Devops_$group --yes #--query "{name:name}"
 done
 
 #az group create -l eastus -n azuredevopsclass
